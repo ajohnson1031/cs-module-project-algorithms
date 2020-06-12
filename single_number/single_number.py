fbd4f9f4f5b2e13deb0767450ee3d285a295a767
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-06-11 22:16:27 -0400

    completed single_number exercise

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,9 +4,21 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
-
+    # choose a method if iterating through the array
+    # rule out numbers that have duplicates
+    numbers_murder = {}
+    i = 0
+    
+    for num in arr:
+        if num in numbers_murder:
+            numbers_murder[num] += 1
+        else:
+            numbers_murder[num] = 1
+            
+    for k, v in numbers_murder.items():
+        if v == 1:
+            return k
+ 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
